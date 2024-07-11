593e882eaf93c1c7ac4af9a3590baa2d0fb2fbcc
Saurabh Brar <saurabh.barar@gmail.com>
2024-07-11 14:35:12 +0530

    update

---
+++ 11-07-2024/module.js
@@ -0,0 +1,38 @@
+/* let sum = require("./sum")
+let total = sum(6,6) 
+console.log(total)*/
+
+/* let fs  = require("fs")
+fs.readFile("./aboutme.txt","utf-8",(err,data)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(data)
+    }
+}) */
+/* console.log("hiiii") */
+
+/* let os = require("os")
+console.log(os.cpus()) */
+
+const { error } = require("console")
+let fs = require("fs")
+
+/* fs.writeFile("./write.txt","Hello I write this in module.js file",(err)=>{
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("File created")
+    }
+}) */
+fs.appendFileSync("./write.txt","  hello this is from append file","utf-8",(callback,error)=>{
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(callback)
+    }
+})
+
---
+++ 11-07-2024/sum.js
@@ -0,0 +1,6 @@
+let sum = (a,b) => {
+    return a+b
+}
+/* console.log(sum(5,6)) */
+
+module.exports = sum
